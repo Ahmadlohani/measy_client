349dcbc907661e29bef6814cdf828eb7e7e728dd
felixCivalero <ah203076@gmail.com>
2022-08-29 15:24:21 +0500

    new things

--- pages/cart/config/keys.js
+++ pages/cart/config/keys.js
@@ -1,6 +1,5 @@
-const keys = {
-    stripePublisherKey:
-      '',
+import React from "react";
+export const keys = {
+	stripePublisherKey:
+		"pk_test_51KJi7TEzIbrc8fRV3I5V8hdE01gSm95GpB7glFogoom9JRhl3lJcJakMBB8gNgS9g94ANH3t5Mh5cqM2FUrgedVO00jmTWcUEG",
 };
-
-export default keys;
